378e7f85254d11fe62383a69ab290ada4a571e3e
Spencer Yue <spenceryue@gmail.com>
2018-04-23 15:14:40 -0500

    build/test cmd line scripts

---
+++ paxos/TestRunner.java
@@ -0,0 +1,19 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Request;
+import org.junit.runner.Result;
+
+public class TestRunner {
+    public static void main(String... args) throws ClassNotFoundException {
+      boolean all_successful = true;
+      for (String arg : args)
+      {
+        String[] classAndMethod = arg.split("#");
+        Request request = Request.method(Class.forName(classAndMethod[0]),
+                classAndMethod[1]);
+
+        Result result = new JUnitCore().run(request);
+        all_successful = all_successful && result.wasSuccessful();
+      }
+      System.exit(all_successful ? 0 : 1);
+    }
+}
